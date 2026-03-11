8de06e1aec4686f42928dfc89829b208693c114d
Patrick <pfriis@outlook.com>
2026-03-11 21:48:12 +0100

    Add LoginScreen with basic layout. TODO: Logic and functionality for logging in

--- app/src/main/java/com/example/birthdaylist/screens/LoginScreen.kt
+++ app/src/main/java/com/example/birthdaylist/screens/LoginScreen.kt
@@ -1,2 +1,114 @@
 package com.example.birthdaylist.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.birthdaylist.NavRoutes
+
+@Composable
+fun LoginScreen(navController: NavHostController) {
+
+    Scaffold(
+        topBar = {},
+    ) {
+        innerPadding ->
+        LoginContent(
+            innerPadding = innerPadding,
+            navController = navController
+        )
+    }
+}
+
+@Composable
+fun LoginContent (
+    innerPadding: PaddingValues,
+    navController: NavController
+) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .padding(innerPadding)
+            .padding(24.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            "Login",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = {Text("Username")},
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = {Text("Password")},
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Button(
+            onClick = {
+                //TODO: Logic for login with API
+                navController.navigate(NavRoutes.Home.route) {
+                    popUpTo(0)
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Login")
+        }
+
+        OutlinedButton(
+            onClick = {
+                //TODO: Logic for create user with API
+                navController.navigate(NavRoutes.Home.route)
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Create New User")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen(
+        navController = rememberNavController()
+    )
+}
