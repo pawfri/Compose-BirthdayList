4a50df673aee1cc92e9b71b9c169db903d7b4a9e
Patrick <pfriis@outlook.com>
2026-03-13 21:49:35 +0100

    Add Friend class for gson annotations

---
+++ app/src/main/java/com/example/birthdaylist/data/Friend.kt
@@ -0,0 +1,17 @@
+package com.example.birthdaylist.data
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Friend(
+    @SerialName("id")
+    // TODO this is kotlinx annotation
+    val id: Int = -1,
+    @SerialName("name")
+    val name: String,
+    @SerialName("birthday")
+    val birthday: String,
+    @SerialName("age")
+    val age: Int? = null
+)
